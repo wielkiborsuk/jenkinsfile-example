0e396563829bd947054ca1da68ca2c517bc58e91
wielki.borsuk <wielki.borsuk@gmail.com>
2019-02-19 00:41:05 +0100

    cleanup jenkins setup - go back to manual preparation

--- jenkins/run.jenkins.sh
+++ jenkins/run.jenkins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home myjenkins
-docker run -p 8080:8080 -p 50000:50000 myjenkins
+docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home myjenkins
+#docker run -p 8080:8080 -p 50000:50000 myjenkins
