6b181629948d05b87600ccdaecfd183d65e333da
wielki.borsuk <wielki.borsuk@gmail.com>
2019-02-19 23:05:45 +0100

    introduce test that randomly fails

--- src/test/java/org/norasoft/jenkinsfile/demo/Demo1Tests.java
+++ src/test/java/org/norasoft/jenkinsfile/demo/Demo1Tests.java
@@ -1,9 +1,12 @@
 package org.norasoft.jenkinsfile.demo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,6 +15,7 @@ public class Demo1Tests {
 	@Test
 	public void contextLoads1() throws InterruptedException {
 		Thread.sleep(5000);
+		Assert.assertTrue(new Random().nextBoolean());
 	}
 
 }
